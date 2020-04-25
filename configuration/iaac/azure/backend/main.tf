b199397079e7b52e9c8afe5d5d804f0da6dfc8a8
idriss-mezguich <54636862+idriss-mezguich@users.noreply.github.com>
2020-04-26 00:14:00 +0100

    Update main.tf

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   version = "=2.0.0"
-  features {
-    key_vault {}
-    }
+  features {}
   client_id       = var.client_id   # ENVIRONMENT VARIABLE
   client_secret   = var.client_secret # ENVIRONMENT VARIABLE
   subscription_id = var.subscription_id
