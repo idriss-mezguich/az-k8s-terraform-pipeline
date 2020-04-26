ec596f18543f528511ba4f5f1b180dba4668627e
idriss-mezguich <54636862+idriss-mezguich@users.noreply.github.com>
2020-04-26 04:31:15 +0100

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   version = "~>2.0.0"
   features {}
-  client_id       = var.client_id   # ENVIRONMENT VARIABLE
-  client_secret   = var.client_secret # ENVIRONMENT VARIABLE
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+#   client_id       = var.client_id   # ENVIRONMENT VARIABLE
+#   client_secret   = var.client_secret # ENVIRONMENT VARIABLE
+#   subscription_id = var.subscription_id
+#   tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "resource_group" {
